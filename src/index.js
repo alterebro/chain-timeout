481b828ed2e7b78fe9ac125c62b33defae7549fa
Jorge Moreno <alterebro@gmail.com>
2019-04-23 14:54:18 +0200

    Module source code

---
+++ src/index.js
@@ -0,0 +1,40 @@
+export default function chainTimeout(fn, t) {
+
+	// SetTimeout Replacement using requestAnimationFrame
+	function requestTimeout(func, delay) {
+		let start = new Date().getTime();
+		let handle = null;
+		let loop = () => { ( (new Date().getTime() - start) >= delay ) ? func.call() : handle = requestAnimationFrame(loop) };
+		    handle = requestAnimationFrame(loop);
+		return handle;
+	}
+
+	let queue = []
+	let self = null;
+	let timer = null;
+
+    function schedule(fn, t) {
+		timer = requestTimeout( function() {
+			timer = null;
+			fn.call();
+		    if (queue.length) {
+                let item = queue.shift();
+                schedule(item.fn, item.t);
+            }
+		}, t);
+    }
+
+    self = {
+        chainTimeout: function(fn, t) {
+			(queue.length || timer) ? queue.push({fn: fn, t: t}) : schedule(fn, t);
+            return self;
+        },
+        clear: function() {
+			cancelAnimationFrame(timer);
+            queue = [];
+            return self;
+        }
+    };
+
+    return self.chainTimeout(fn, t);
+}
